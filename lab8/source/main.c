c5d32bdba2455595a032741b61568707703d3a8e
sspon001 <sspon001@ucr.edu>
2021-11-14 11:37:38 -0800

    part3 done

--- lab8/source/main.c
+++ lab8/source/main.c
@@ -1,7 +1,7 @@
 /*	Author: sspon001
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Assignment: Lab 8  Exercise 1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -12,12 +12,24 @@
 #include "simAVRHeader.h"
 #endif
 
+unsigned short x = 0x00 ;
+void ADC_init(){
+	ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE) ;
+}
+
 int main(void) {
-    /* Insert DDR and PORT initializations */
-
-    /* Insert your solution below */
+	DDRA = 0x00 ; PORTA = 0xFF ;
+	DDRB = 0xFF ; PORTB = 0x00 ;
+	DDRC = 0xFF ; PORTC = 0x00 ;
+	DDRD = 0xFF ; PORTD = 0x00 ;
+	ADC_init() ;
+	unsigned short max = 0x8A ;
+	unsigned short min = 0x30 ;
     while (1) {
-
+	    x = ADC ;
+	    PORTB = 0x00 ;
+	    if(x >= (max/2)) PORTB = 0x01 ;
+	    else PORTB = 0x00 ;
     }
     return 1;
 }
